e8df62748b73ec2c1677b44994c62ebdfa04b25f
rezaul-karim <rezaul.coderpro@gmail.com>
2026-06-23 18:08:22 +0600

    update base url

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",
+
+  server: {
+    historyApiFallback: true,
+    port: 4000,
+  },
 });
